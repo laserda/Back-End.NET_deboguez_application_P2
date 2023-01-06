497faad2ac6682f6f74ba8e3b093fbf0bb774e68
Reindeer85 <92531724+Reindeer85@users.noreply.github.com>
2023-01-06 11:23:16 +0100

    Add files via upload

---
+++ P2FixAnAppDotNetCode/Components/ResumePanierViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P2FixAnAppDotNetCode.Models;
+
+namespace P2FixAnAppDotNetCode.Components
+{
+    public class ResumePanierViewComponent : ViewComponent
+    {
+        private readonly Panier _panier;
+
+        public ResumePanierViewComponent(IPanier panier)
+        {
+            _panier = panier as Panier;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_panier);
+        }
+    }
+}
---
+++ P2FixAnAppDotNetCode/Components/SelectionLangageViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P2FixAnAppDotNetCode.Models.Services;
+
+namespace P2FixAnAppDotNetCode.Components
+{
+    public class SelectionLangageViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ILangageService langageService)
+        {
+            return View(langageService);
+        }
+    }
+}
