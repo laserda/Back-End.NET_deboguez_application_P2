68833a767d87a2565eb4663a729c2c533f0b4cb9
Reindeer85 <92531724+Reindeer85@users.noreply.github.com>
2023-01-06 11:34:30 +0100

    Delete IProductRepository.cs

--- P2FixAnAppDotNetCode/Models/Repositories/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P2FixAnAppDotNetCode.Models.Repositories
-{
-    public interface IProductRepository
-    {
-        Product[] GetAllProducts();
-
-        void UpdateProductStocks(int productId, int quantityToRemove);
-    }
-}
