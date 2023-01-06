e2b8dcad874fb580ecb08ec8856f344b24ea9e9c
Reindeer85 <92531724+Reindeer85@users.noreply.github.com>
2023-01-06 11:37:07 +0100

    Delete IProductService.cs

--- P2FixAnAppDotNetCode/Models/Services/IProductService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P2FixAnAppDotNetCode.Models.Services
-{
-    public interface IProductService
-    {
-        Product[] GetAllProducts();
-        Product GetProductById(int id);
-        void UpdateProductQuantities(Cart cart);
-    }
-}
